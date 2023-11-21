020d1c24f79b1fe2e5807241090ca5d78cf0cbc9
bwjason <jasonkim5672@gmail.com>
2023-11-21 22:41:06 +0900

    [ADD] build to docs

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,28 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
   ],
+  base: "/wedding-invitaion",
+  build: {
+    assetsDir: ".",
+    rollupOptions: {
+      input: "./index.html",
+      output: {
+        dir: "docs",
+      },
+    },
+  },
   resolve: {
+
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+
 })
