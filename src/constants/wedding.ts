ae01aacc3ce5dccbac5454866790963ed5fbb82d
jason <bwjason.kim@samsung.com>
2023-11-28 12:54:20 +0900

    [ADD] MAIN PAGE

---
+++ src/constants/wedding.ts
@@ -0,0 +1,6 @@
+export const WEDDING = {
+  DAY: '2024-03-09',
+  TIME: '17:30',
+  BROOM: 'JASON',
+  BRIDE: 'JENNY'
+}
---
+++ src/views/TitleView.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import {WEDDING} from "@/constants/wedding";
+
+const weddingDate: string = WEDDING.DAY + ' ' + WEDDING.TIME
+
+
+</script>
+
+<template>
+  <div>
+    {{ weddingDate }}
+  </div>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
