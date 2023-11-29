57921fceacfc8a8a29c637bbc88417f2e81f8528
bwjason <jasonkim5672@gmail.com>
2023-11-29 18:38:41 +0900

    [ADD] scss 적용

--- src/views/TitleView.vue
+++ src/views/TitleView.vue
@@ -1,18 +1,15 @@
 <script setup lang="ts">
-import {WEDDING} from "@/constants/wedding";
+import { WEDDING } from '@/constants/wedding'
 
 const weddingDate: string = WEDDING.DAY + ' ' + WEDDING.TIME
-
-
 </script>
 
 <template>
-  <div>
+  <div class="d-flex bg-black h-500px justify-center align-center">
     {{ weddingDate }}
   </div>
 </template>
 
-
 <style lang="scss" scoped>
-
+@import '/styles/common.scss';
 </style>
