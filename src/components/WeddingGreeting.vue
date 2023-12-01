52c62e296b773e54f3cd5912772e07a67de6a84b
jason <bwjason.kim@samsung.com>
2023-12-01 18:24:37 +0900

    [FIX] git actions test

--- src/components/WeddingGreeting.vue
+++ src/components/WeddingGreeting.vue
@@ -1,16 +1,16 @@
 <template>
   <div
-    class="bg-black d-flex flex-column w-100 h-200px align-center justify-center font-easta-heavy"
+      class="bg-black d-flex flex-column w-100 h-200px align-center justify-center font-easta-heavy"
   >
     <div>
-      <div class="align-content-start sub-title">초대합니다.</div>
+      <div class="align-content-start sub-title">초대합니다</div>
 
-      <br />
+      <br/>
       <div class="greeting-content">
-        함께하는 순간들이 소중한 저희가<br />
-        평생의 동반자로 새로운 시작을 약속하려 합니다.<br />
-        귀한 걸음 하시어 그 시작의 자리에 함께해 주시면<br />
-        더 없는 기쁨으로 간직하겠습니다.<br />
+        함께하는 순간들이 소중한 저희가<br/>
+        평생의 동반자로 새로운 시작을 약속하려 합니다.<br/>
+        귀한 걸음 하시어 그 시작의 자리에 함께해 주시면<br/>
+        더 없는 기쁨으로 간직하겠습니다.<br/>
       </div>
     </div>
   </div>
@@ -20,8 +20,10 @@
 
 <style lang="scss" scoped>
 @import '/src/styles/common.scss';
+
 .greeting-title {
 }
+
 .greeting-content {
   font-size: 0.8rem;
 }
