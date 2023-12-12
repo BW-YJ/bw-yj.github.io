5ecac115e46f10c2eca26d3d842e6e7eb3f6d665
jason <bwjason.kim@samsung.com>
2023-12-12 15:00:59 +0900

    [FIX] 커스텀 오버레이 적용

--- src/components/WeddingHallMap.vue
+++ src/components/WeddingHallMap.vue
@@ -1,21 +1,22 @@
 <template>
+
   <div class="d-flex justify-center flex-column align-center ga-5 pa-10 font-easta-heavy">
     <div class="d-flex flex-row w-100">
       <div class="sub-title">오시는길</div>
     </div>
     <div class="map_wrap">
       <div
-        id="map"
-        class="mobile-map"
+          id="map"
+          class="mobile-map"
       ></div>
       <div class="custom_zoomcontrol radius_border">
         <span :onClick="zoomIn"
-          ><img
+        ><img
             src="https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/ico_plus.png"
             alt="확대"
         /></span>
         <span :onClick="zoomOut"
-          ><img
+        ><img
             src="https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/ico_minus.png"
             alt="축소"
         /></span>
@@ -25,7 +26,8 @@
 </template>
 
 <script setup>
-import { onMounted, ref } from 'vue'
+import {nextTick, onMounted, ref} from 'vue'
+
 let map = null
 const container = ref(null)
 const weddingPosition = ref(null)
@@ -72,11 +74,11 @@ const placesSearchCB = (data, status, pagination) => {
 const displayMarker = (place) => {
   weddingPosition.value = place
   var imageSrc = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png',
-    imageSize = new window.kakao.maps.Size(64, 69),
-    imageOption = { offset: new window.kakao.maps.Point(27, 69) }
+      imageSize = new window.kakao.maps.Size(32, 34.5),
+      imageOption = {offset: new window.kakao.maps.Point(13.5, 34)}
 
   var markerImage = new window.kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
-    markerPosition = new window.kakao.maps.LatLng(place.y, place.x)
+      markerPosition = new window.kakao.maps.LatLng(place.y, place.x)
 
   var marker = new window.kakao.maps.Marker({
     position: markerPosition,
@@ -86,18 +88,57 @@ const displayMarker = (place) => {
 
   //TODO : 마커 정리하기
   var overayContent =
-    '<div class="customoverlay">' +
-    '  <a href="https://map.naver.com/p/entry/place/1897374821?c=15.00,0,0,0,dh" target="_blank">' +
-    '    <span class="title">JK아트컨벤션</span>' +
-    '  </a>' +
-    '</div>'
+      '<div ref="customOverlay" class="customoverlay">' +
+      '  <a href="https://map.naver.com/p/entry/place/1897374821?c=15.00,0,0,0,dh" target="_blank">' +
+      '    <span class="title">JK아트컨벤션</span>' +
+      '  </a>' +
+      '</div>'
 
   var customOverlay = new window.kakao.maps.CustomOverlay({
-    map:map,
+    map: map,
     position: markerPosition,
     content: overayContent,
     yAnchor: 1
   })
+  nextTick(() => {
+    var customOverlayElement = document.querySelector('.customoverlay');
+    var customLinkElement = document.querySelector('.customoverlay a');
+    var customTitleElement = document.querySelector('.customoverlay .title');
+
+    if (customLinkElement) {
+      customLinkElement.style.display = 'block';
+      customLinkElement.style.textDecoration = 'none';
+      customLinkElement.style.color = '#000';
+      customLinkElement.style.textAlign = 'center';
+      customLinkElement.style.borderRadius = '6px';
+      customLinkElement.style.fontSize = '10px';
+      customLinkElement.style.fontWeight = 'bold';
+      customLinkElement.style.overflow = 'hidden';
+      customLinkElement.style.background = '#d95050';
+      customLinkElement.style.backgroundImage = 'url(https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/arrow_white.png)';
+      customLinkElement.style.backgroundPosition = 'right 3px center';
+      customLinkElement.style.backgroundRepeat = 'no-repeat';
+    }
+
+    if (customTitleElement) {
+      customTitleElement.style.display = 'block';
+      customTitleElement.style.textAlign = 'center';
+      customTitleElement.style.background = '#fff';
+      customTitleElement.style.marginRight = '15px';
+      customTitleElement.style.padding = '2px 5px';
+      customTitleElement.style.fontSize = '14px';
+      customTitleElement.style.fontWeight = 'bold';
+    }
+    if (customOverlayElement) {
+      customOverlayElement.style.position = 'relative';
+      customOverlayElement.style.bottom = '40px';
+      customOverlayElement.style.right = '5px';
+      customOverlayElement.style.borderRadius = '6px';
+      customOverlayElement.style.border = '1px solid #ccc';
+      customOverlayElement.style.borderBottom = '2px solid #ddd';
+      customOverlayElement.style.float = 'left';
+    }
+  });
 }
 
 const zoomIn = () => {
@@ -118,10 +159,12 @@ const zoomOut = () => {
   width: 100%;
   height: 350px;
 }
+
 .radius_border {
   border: 1px solid #919191;
   border-radius: 5px;
 }
+
 .custom_zoomcontrol {
   position: absolute;
   top: 50px;
@@ -132,6 +175,7 @@ const zoomOut = () => {
   z-index: 1;
   background-color: #f5f5f5;
 }
+
 .custom_zoomcontrol span {
   display: block;
   width: 36px;
@@ -140,24 +184,14 @@ const zoomOut = () => {
   cursor: pointer;
   padding: 10px;
 }
+
 .custom_zoomcontrol span img {
   width: 15px;
   height: 15px;
   border: none;
 }
+
 .custom_zoomcontrol span:first-child {
   border-bottom: 1px solid #bfbfbf;
 }
-.customoverlay {
-  position:relative;
-  bottom:85px;
-  border-radius:6px;
-  border: 1px solid #ccc;
-  border-bottom:2px solid #ddd;
-  float:left;
-}
-.customoverlay:nth-of-type(n) {border:0; box-shadow:0px 1px 2px #888;}
-.customoverlay a {display:block;text-decoration:none;color:#000;text-align:center;border-radius:6px;font-size:14px;font-weight:bold;overflow:hidden;background: #d95050;background: #d95050 url(https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/arrow_white.png) no-repeat right 14px center;}
-.customoverlay .title {display:block;text-align:center;background:#fff;margin-right:35px;padding:10px 15px;font-size:14px;font-weight:bold;}
-.customoverlay:after {content:'';position:absolute;margin-left:-12px;left:50%;bottom:-12px;width:22px;height:12px;background:url('https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/vertex_white.png')}
 </style>
