47908edd8ebc68c3b504b0c9803e50c9abf25184
bwjason <jasonkim5672@gmail.com>
2023-12-15 16:34:13 +0900

    [ADD] ZOOM IN/OUT BUTTON ON KAKAO MAP

--- src/components/WeddingGreeting.vue
+++ src/components/WeddingGreeting.vue
@@ -1,15 +1,18 @@
 <template>
-  <div data-aos="fade-left" data-aos-duration="1500"
-       class="bg-black d-flex flex-column w-100 h-200px font-easta-heavy pa-10">
+  <div
+    data-aos="fade-up"
+    data-aos-duration="1500"
+    class="bg-black d-flex flex-column w-100 h-200px font-easta-heavy pa-10"
+  >
     <div>
       <div class="sub-title">초대합니다</div>
 
-      <br/>
+      <br />
       <div class="greeting-content">
-        함께하는 순간들이 소중한 저희가<br/>
-        평생의 동반자로 새로운 시작을 약속하려 합니다.<br/>
-        귀한 걸음 하시어 그 시작의 자리에 함께해 주시면<br/>
-        더 없는 기쁨으로 간직하겠습니다.<br/>
+        함께하는 순간들이 소중한 저희가<br />
+        평생의 동반자로 새로운 시작을 약속하려 합니다.<br />
+        귀한 걸음 하시어 그 시작의 자리에 함께해 주시면<br />
+        더 없는 기쁨으로 간직하겠습니다.<br />
       </div>
     </div>
   </div>
