1452878343d37a2e1c916df4ceffd3f40b3dc59e
bwjason <jasonkim5672@gmail.com>
2023-12-20 17:20:00 +0900

    [ADD] 캘린더 기능 추가

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,15 @@ import vuetify from './plugins/vuetify'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 import { loadFonts } from './plugins/webfontloader'
+import VCalendar from 'v-calendar'
+import 'v-calendar/style.css'
 
 loadFonts()
 
 createApp(App)
   .use(router)
   .use(vuetify)
+  .use(VCalendar, {})
   .mixin({
     created() {
       AOS.init()
