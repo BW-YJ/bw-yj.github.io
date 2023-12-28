426a0b9482af3e2afdd785fe72fb240b37def2bb
jason <bwjason.kim@samsung.com>
2023-12-28 14:44:14 +0900

    [FIX]url 수정

--- src/components/WeddingAdditional.vue
+++ src/components/WeddingAdditional.vue
@@ -1,19 +1,20 @@
 <template>
   <div class="d-flex flex-column w-100 font-easta-heavy">
     <v-banner
-      class="banner"
-      lines="one"
-      text="간편하게 예식일에 맞춰 축하 화환을 전해보세요. "
-      stacked
-      sticky
+        class="banner"
+        lines="one"
+        text="간편하게 예식일에 맞춰 축하 화환을 전해보세요. "
+        stacked
+        sticky
     >
       <template v-slot:actions>
         <v-btn
-          variant="outlined"
-          color="deep-blue-accent-4"
-          href="https://agent.maruw.co.kr/product?shop=bojagicard&idx=e75f1236187d42c719996f84495b7b34"
-          target="_blank"
-          >바로 주문</v-btn
+            variant="outlined"
+            color="deep-blue-accent-4"
+            href="http://agent.maruw.co.kr/mobile/product?shop=bojagicard&idx=71a17b723e677a2371527fa088e726dd"
+            target="_blank"
+        >바로 주문
+        </v-btn
         >
       </template>
     </v-banner>
@@ -21,11 +22,13 @@
 </template>
 
 <script setup>
-const moveToPresent = () => {}
+const moveToPresent = () => {
+}
 </script>
 
 <style lang="scss" scoped>
 @import '/src/styles/common.scss';
+
 .banner {
   background: #0f172a;
   color: white;
