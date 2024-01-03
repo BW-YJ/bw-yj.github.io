96af746e3402c4ee14d0a429acc5251b2e3b0a29
bwjason <jasonkim5672@gmail.com>
2024-01-04 00:48:22 +0900

    [REMOVE] 화환 배너 제거

--- src/components/WeddingAdditional.vue
+++ src/components/WeddingAdditional.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="d-flex flex-column w-100 font-easta-heavy align-center ga-10">
-    <div class="d-flex flex-row justify-center w-80">
+    <div class="d-flex flex-row justify-center w-80 pb-10">
       <v-btn
         block
         variant="outlined"
@@ -14,23 +14,23 @@
       </v-btn>
     </div>
 
-    <v-banner
-      class="banner"
-      lines="one"
-      text="간편하게 예식일에 맞춰 축하 화환을 전해보세요. "
-      stacked
-      sticky
-    >
-      <template v-slot:actions>
-        <v-btn
-          variant="outlined"
-          color="deep-blue-accent-4"
-          href="http://agent.maruw.co.kr/mobile/product?shop=bojagicard&idx=71a17b723e677a2371527fa088e726dd"
-          target="_blank"
-          >바로 주문
-        </v-btn>
-      </template>
-    </v-banner>
+    <!--    <v-banner-->
+    <!--      class="banner"-->
+    <!--      lines="one"-->
+    <!--      text="간편하게 예식일에 맞춰 축하 화환을 전해보세요. "-->
+    <!--      stacked-->
+    <!--      sticky-->
+    <!--    >-->
+    <!--      <template v-slot:actions>-->
+    <!--        <v-btn-->
+    <!--          variant="outlined"-->
+    <!--          color="deep-blue-accent-4"-->
+    <!--          href="http://agent.maruw.co.kr/mobile/product?shop=bojagicard&idx=71a17b723e677a2371527fa088e726dd"-->
+    <!--          target="_blank"-->
+    <!--          >바로 주문-->
+    <!--        </v-btn>-->
+    <!--      </template>-->
+    <!--    </v-banner>-->
   </div>
 </template>
 
